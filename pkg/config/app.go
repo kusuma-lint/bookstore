a26db6da9e8d340d9ec13286a9f60ad1471af739
Kusuma Lestari <75372340+kusuma-lint@users.noreply.github.com>
2023-09-18 09:48:57 +0700

    first commit

---
+++ pkg/config/app.go
@@ -0,0 +1,23 @@
+package config
+
+import (
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+)
+
+var (
+	db *gorm.DB
+)
+
+func Connect() {
+	d, err := gorm.Open("mysql", "user:password@/dbname?charset=utf8&parseTime=True&loc=Local")
+	if err != nil {
+		panic(err)
+	}
+	db = d
+}
+
+func GetDB() *gorm.DB {
+	return db
+}
